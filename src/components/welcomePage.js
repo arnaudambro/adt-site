79a06f0621dfcd63f2b930732b6c868327cdabf9
arnaudambro <arnaud.ambroselli@yahoo.fr>
2020-10-23 10:53:03 +0200

    fixing video

--- src/components/welcomePage.js
+++ src/components/welcomePage.js
@@ -13,6 +13,9 @@ import useLargeImages from "../helpers/hooks/useLargeImages"
 import useWindowSize from "../helpers/hooks/useWindowSize"
 import useMP4 from "../helpers/hooks/useMP4"
 
+const videoDim = { height: 1080, width: 1920 }
+const getRatio = ({ width, height }) => width / height // 16/9 for the video
+
 const LandingStyled = styled(Link)`
   z-index: 1000;
   overflow: hidden;
@@ -66,27 +69,28 @@ const VideoSubContainer = styled.div`
   width: 100vw;
 `
 
+const posterWidth = 100
 const PosterContainer = styled.div`
   position: absolute;
+  width: ${posterWidth}vw;
   top: 0;
   left: 0;
-  bottom: 0;
-  right: 0;
+  transform: scale(1.1);
+  height: ${posterWidth / getRatio(videoDim)}vw;
 `
 
 const VideoContainer = styled.div`
   position: relative;
-  min-width: ${props => props.videoContainerWidth};
-  width: ${props => props.videoContainerWidth};
-  height: 110vh;
+  min-width: ${props => props.width};
+  width: ${props => props.width};
+  height: ${props => props.height};
   @media all and (min-width: 700px) {
     .video {
       position: absolute;
       top: 0;
       left: 0;
-      width: 100%;
-      height: 100%;
-      min-height: 100vh;
+      width: 110vw;
+      height: ${110 / getRatio(videoDim)}vw;
     }
   }
 `
@@ -158,19 +162,25 @@ const landingPageNumber = (offset = 0) => {
   }
 }
 
-const videoDim = { height: 1080, width: 1920 }
-const getRatio = ({ width, height }) => width / height // 16/9 for the video
+const getVideoContainerDims = windowDim => {
+  if (getRatio(windowDim) < getRatio(videoDim)) {
+    const width = windowDim.height * getRatio(videoDim) + 100
+    return {
+      width: `${width}px !important`,
+      height: `${width / getRatio(videoDim)}px !important`,
+    }
+  }
+  const width = 110
+  return {
+    width: `${width}vw`,
+    height: `${width / getRatio(videoDim)}vw`,
+  }
+}
 
 const WelcomePage = ({ withAnimation }) => {
   const images = useLargeImages()
   const windowDim = useWindowSize()
   const video = useMP4()
-  const videoContainerWidth =
-    getRatio(windowDim) < getRatio(videoDim)
-      ? `${windowDim.height * getRatio(videoDim) + 100}px !important`
-      : "110vw"
-
-  console.log({ withAnimation })
   const renderVideo = () => {
     if (windowDim.width < 700) return null
     return (
@@ -184,12 +194,11 @@ const WelcomePage = ({ withAnimation }) => {
         </PosterContainer>
         {!withAnimation && (
           <VideoContainer
-            videoContainerWidth={videoContainerWidth}
+            {...getVideoContainerDims(windowDim)}
             dangerouslySetInnerHTML={{
-              __html: `<video class="video" muted autoplay loop preload src=${video} poster=${getImageFromSrc(
-                images,
-                "poster.png"
-              )}></video>`,
+              __html: `<video class="video" muted autoplay loop preload src=${video} poster=${
+                getImageFromSrc(images, "poster.png").src
+              }></video>`,
             }}
           />
         )}
