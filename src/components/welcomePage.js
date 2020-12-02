1dc5ee7b16a92c38fd40de347c850365308fd1da
arnaudambro <arnaud.ambroselli@yahoo.fr>
2020-12-02 14:41:35 +0100

    remove video

--- src/components/welcomePage.js
+++ src/components/welcomePage.js
@@ -10,11 +10,6 @@ import { Link } from "gatsby"
 import { projets } from "../reference/pages"
 import { background, gatsbyImage } from "../styles/mixins"
 import useLargeImages from "../helpers/hooks/useLargeImages"
-import useWindowSize from "../helpers/hooks/useWindowSize"
-import useMP4 from "../helpers/hooks/useMP4"
-
-const videoDim = { height: 1080, width: 1920 }
-const getRatio = ({ width, height }) => width / height // 16/9 for the video
 
 const LandingStyled = styled(Link)`
   z-index: 1000;
@@ -61,40 +56,6 @@ const ArrowDownStyled = styled.svg`
   animation-iteration-count: infinite;
 `
 
-const VideoSubContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-  width: 100vw;
-`
-
-const posterWidth = 100
-const PosterContainer = styled.div`
-  position: absolute;
-  width: ${posterWidth}vw;
-  top: 0;
-  left: 0;
-  transform: scale(1.1);
-  height: ${posterWidth / getRatio(videoDim)}vw;
-`
-
-const VideoContainer = styled.div`
-  position: relative;
-  min-width: ${props => props.width};
-  width: ${props => props.width};
-  height: ${props => props.height};
-  @media all and (min-width: 700px) {
-    .video {
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 110vw;
-      height: ${110 / getRatio(videoDim)}vw;
-    }
-  }
-`
-
 const Logo = styled.div`
   position: absolute;
   ${background({ url: logo })}
@@ -139,6 +100,7 @@ const ScrollIcon = () => (
   </ArrowDownStyled>
 )
 
+const numberOfImages = 3
 const landingPageNumber = (offset = 0) => {
   const oneDayInMs = 1000 * 60 * 60 * 24
   const now = Date.now() - offset * oneDayInMs
@@ -149,7 +111,6 @@ const landingPageNumber = (offset = 0) => {
     nowDate.getDate()
   )
   const changeFrequency = 3
-  const numberOfImages = 4
   const nowZeroInDays = Math.floor(Date.parse(nowZero) / oneDayInMs)
   const parsedPageNumber =
     (nowZeroInDays % (changeFrequency * numberOfImages)) + 1
@@ -162,62 +123,17 @@ const landingPageNumber = (offset = 0) => {
   }
 }
 
-const getVideoContainerDims = windowDim => {
-  if (getRatio(windowDim) < getRatio(videoDim)) {
-    const width = windowDim.height * getRatio(videoDim) + 100
-    return {
-      width: `${width}px !important`,
-      height: `${width / getRatio(videoDim)}px !important`,
-    }
-  }
-  const width = 110
-  return {
-    width: `${width}vw`,
-    height: `${width / getRatio(videoDim)}vw`,
-  }
-}
-
-const WelcomePage = ({ withAnimation }) => {
+const WelcomePage = () => {
   const images = useLargeImages()
-  const windowDim = useWindowSize()
-  const video = useMP4()
-  const renderVideo = () => {
-    if (windowDim.width < 700) return null
-    if (landingPageNumber().landingPageNumber !== 4) return null
-    return (
-      <VideoSubContainer>
-        <PosterContainer>
-          <Img
-            fluid={getImageFromSrc(images, "poster.png")}
-            alt="ADT: une mise en architecture de la matière"
-            title="ADT: une mise en architecture de la matière"
-          />
-        </PosterContainer>
-        {!withAnimation && (
-          <VideoContainer
-            {...getVideoContainerDims(windowDim)}
-            dangerouslySetInnerHTML={{
-              __html: `<video class="video" muted autoplay loop preload src=${video} poster=${
-                getImageFromSrc(images, "poster.png").src
-              }></video>`,
-            }}
-          />
-        )}
-      </VideoSubContainer>
-    )
-  }
 
   const renderImage = () => {
-    const photoNumber = landingPageNumber().landingPageNumber
-    if (photoNumber === 4) {
-      if (windowDim.width > 700) return null
-    }
+    const photoNumber = Math.min(
+      landingPageNumber().landingPageNumber,
+      numberOfImages
+    )
     return (
       <Img
-        fluid={getImageFromSrc(
-          images,
-          `CONCEPT-PDG${photoNumber === 4 ? 3 : photoNumber}.jpg`
-        )}
+        fluid={getImageFromSrc(images, `CONCEPT-PDG${photoNumber}.jpg`)}
         alt="ADT: une mise en architecture de la matière"
         title="ADT: une mise en architecture de la matière"
       />
@@ -228,7 +144,6 @@ const WelcomePage = ({ withAnimation }) => {
     <LandingStyled to={`/${projets}`} id="landing">
       <SEO title="ADT" />
       <PageNumber>{JSON.stringify(landingPageNumber(), null, 2)}</PageNumber>
-      {renderVideo()}
       {renderImage()}
       <ScrollIcon />
       <Logo />
